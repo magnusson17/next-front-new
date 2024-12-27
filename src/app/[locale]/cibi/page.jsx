fa36aa2a1291da813859962ee3a264bede8e479b
Gabriele Magni <gabri.magni.97@gmail.com>
2024-12-27 14:07:46 +0100

    test7

--- src/app/[locale]/cibi/page.jsx
+++ src/app/[locale]/cibi/page.jsx
@@ -7,62 +7,62 @@ export default async function Cibi({ params }) {
     const {locale} = await params
     const singlePage = await getSingleContent(process.env.GET_ALL_PAGES, locale, "nid", 15)
     const blocchiIds = await arrayIdBlocchi(singlePage)
-    const food = await fetchRequest(process.env.GET_ALL_FOOD, locale)
-    // per il filtro
+    const foods = await fetchRequest(process.env.GET_ALL_FOOD, locale)
+    // per looppare i cibi di cat in cat e per il filtro cat
     const titoliCategorie = await fetchRequest(process.env.GET_ALL_CATEGORIE_CIBO, locale)
 
     return (
         <main className="relative page-pizze">
-            {/* <FiltroCibo titoliCat={titoliCategorie} /> */}
-            <div className="
-            teasers-cibi
-            px-10
-            ">
-                {Object.entries(categorizedFood).map(([cat, price]) => (
-                    <div key={cat}>
-                        <h2 
-                        id={cat.toLowerCase()}
-                        className="
-                        text-[30px]
-                        font-bold
-                        uppercase
-                        mt-10
-                        ">{cat}</h2>
-                        {Object.entries(price).map(([price, els]) => (
-                            <div key={price}>
-                                <h3 className="
-                                text-base
-                                text-white
-                                font-bold
-                                uppercase
-                                bg-black
-                                py-2
-                                pl-10
-                                -ml-10
-                                ">{price}</h3>
-                                <ul className="
-                                bg-primary-bg
-                                pl-10
-                                -ml-10
-                                ">
-                                    {els.map(el => (
-                                        <li key={el.nid}>
-                                            <h4 className="
-                                            text-red-700
+            <FiltroCibo titoliCat={titoliCategorie} />
+
+            <div className="teasers-cibi px-10">
+                {titoliCategorie.map(cat => {
+                    return (
+                        <div id={cat.name.toLowerCase()} key={cat.name}>
+                            <h2 className="text-[30px] font-bold uppercase mt-10">
+                                {cat.name}
+                            </h2>
+
+                            {foods
+                                .filter((el) => el.field_categoria.includes(cat.name))
+                                .map((el) => {
+                                    return (
+                                        <div key={el.nid}>
+                                            <h3 className="
+                                            text-base
+                                            text-white
                                             font-bold
                                             uppercase
-                                            ">{el.field_titolo_main}</h4>
-                                            <p className="
-                                            lowercase
-                                            ">{el.field_ingredienti}</p>
-                                        </li>
-                                    ))}
-                                </ul>
-                            </div>
-                        ))}
-                    </div>
-                ))}
+                                            bg-black
+                                            py-2
+                                            pl-10
+                                            -ml-10
+                                            ">{el.field_prezzo}</h3>
+                                            <ul className="
+                                            bg-primary-bg
+                                            pl-10
+                                            -ml-10
+                                            ">
+                                                <li>
+                                                    <h4 className="
+                                                    text-red-700
+                                                    font-bold
+                                                    uppercase
+                                                    ">{el.field_titolo_main}</h4>
+                                                    <p className="
+                                                    lowercase
+                                                    ">{el.field_ingredienti}</p>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    )
+                                })
+                            }
+                        </div>
+                    )
+                })}
             </div>
+
             {blocchiIds ? <AllBlocchi blocchiIds={blocchiIds} lang={locale} /> : ''}
         </main>
     );
